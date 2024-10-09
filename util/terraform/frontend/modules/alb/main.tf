7acf3b8232d39081116ffe997a1a63f62ba0dea7
cebarahona <cesar.barahona@varsity.dev>
2024-10-08 22:48:16 -0600

    added target groups to alb listener

--- util/terraform/frontend/modules/alb/main.tf
+++ util/terraform/frontend/modules/alb/main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_default_vpc" "default" {
   tags = {
@@ -25,7 +28,19 @@ resource "aws_lb_listener" "http" {
     protocol = "HTTP"
      default_action {
         type             = "forward"
+              forward {
+        target_group {
+          arn    = aws_lb_target_group.green.arn
+          weight = 0
+        }
+
+        target_group {
+          arn    = aws_lb_target_group.blue.arn
+          weight = 100
+        }
+      }
         target_group_arn = aws_lb_target_group.ec2.arn
+        #target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:739685044447:targetgroup/anyhasher-tg-01/16ad37833599ce1f"
   }
 }
 
@@ -46,21 +61,4 @@ resource "aws_security_group" "alb" {
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
-}
-
-resource "aws_lb_target_group" "ec2" {
-    name       = "anyhasher-tg"
-    port       = 80
-    protocol   = "HTTP"
-    vpc_id     = aws_default_vpc.default.id
-    health_check {
-        path                = "/"
-        protocol            = "HTTP"
-        matcher             = 200
-        interval            = 15
-        timeout             = 3
-        healthy_threshold   = 2
-        unhealthy_threshold = 2
-    }
-
 }
